8767f8819b393aa94f7f767e43b0526a8ff598b6
AnywayMax <83577379+AnywayMax@users.noreply.github.com>
2022-09-08 21:36:56 +0300

    Update crestik.py

--- crestik.py
+++ crestik.py
@@ -1,6 +1,3 @@
-gameboard = [[" "] * 3 for i in range(3)]
-player=None
-
 def win_or_loose():
     win_combination = [((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
                        ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
@@ -9,22 +6,30 @@ def win_or_loose():
     for a, b, c in win_combination:
         if gameboard[a[0]][a[1]] == gameboard[b[0]][b[1]] == gameboard[c[0]][c[1]] != ' ':
             print(f'Выиграл {gameboard[a[0]][a[1]]}!')
-            return True
+            print('Выйти из игры? ')
+            answer = input()
+            if answer in ('да', 'Да', 'yes', 'Yes'):
+                exitgame()
     return False
 
+def exitgame(): 
+    print('Выход из игры!')
+    SystemExit(1)
+    
 def gameboard_print (gameboard):
-  print ('  0-1-2')
+  print ('   0-1-2')
   for row in range (len(gameboard)):
-    print (str(row)+'|', *gameboard[row])
+    print (str(row)+']', *gameboard[row])
     
     
 def select_player (gameboard,player):
   while True:
     gameboard_print(gameboard)
-    choise = input (f'Ход {player}! Выберите клетку путем ввода двух координат от 0 до 2: ').split()
-    print("\033[H\033[J")
+    print(f'Ход игрока [{player}]!')
+    choise = input (f'Выберите клетку [путем ввода двух координат [0..2 0..2]: ').split()
+    print("\033[H\033[J") #очистить консоль для новой отрисовки игровой доски
     if len(choise) != 2:
-      print ('Введите 2 координаты: ')
+      print ('Введите 2 координаты[0..2 0..2]: ')
       continue
     if not choise[0].isdigit() or not choise[1].isdigit():
       print('Неверный выбор. Нужны числа. ')
@@ -49,8 +54,12 @@ def game (gameboard,player):
       player = 'o'
     x, y = select_player(gameboard, player)
     if count == 9:
-      print('Ничья')
+      print('Ничья! Достигнуто максимальное количество ходов!')
       break
     gameboard[x][y] = player
+
+gameboard = [[" "] * 3 for i in range(3)]
+
+player=None    
     
 game (gameboard,player)
